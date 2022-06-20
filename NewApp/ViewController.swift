3b32b91e9649518cdc96237e0e22fde3116b084b
Luiza <khaj-luizkin@yandex.ru>
2022-06-20 20:54:02 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Луиза Самойленко on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
