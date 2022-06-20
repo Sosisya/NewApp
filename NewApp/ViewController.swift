a6e87e19de846e5bdbb05e314716a52ef27dec49
Luiza <khaj-luizkin@yandex.ru>
2022-06-20 21:05:43 +0300

    ddd

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hi")
         print("")
+
+        print("")
         print("")
         // Do any additional setup after loading the view.
     }
